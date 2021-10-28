755cbc598d68cdc327e1a9d3fa61bf818e1dbfb2
Martin Wheeler <m87.wheeler@gmail.com>
2021-10-28 15:21:34 +0100

    chore: add animation to compass

--- src/components/map/compass/styles.ts
+++ src/components/map/compass/styles.ts
@@ -1,8 +1,26 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const SCALE_IN = keyframes`
+    60% {
+        transform: scale(1.1);
+    } 
+    75% {
+        transform: scale(.95);
+    }
+    90% {
+        transform: scale(1.05);
+    }
+    100% {
+        transform: scale(1);
+    }
+`;
 
 export const Container = styled.div`
   position: absolute;
   top: 48%;
   left: 20%;
   width: 7.5%;
+  transform: scale(0);
+  transform-origin: center;
+  animation: ${SCALE_IN} 0.5s linear forwards 2s;
 `;
